7f97555ba4134a981572c07d2788d829e22b9e55
david-vargas98 <edgar981125@live.com.mx>
2025-08-04 01:06:29 -0600

    feat: vpc file.
    Making modifications to vpc.tf file by adding elastic ips, nat gateways, route tables, table associations, and adding some modifications to security group for inbound traffic as a self-reference to allow communicate services.

--- chapter_5/work_5/terraform/vpc.tf
+++ chapter_5/work_5/terraform/vpc.tf
@@ -16,86 +16,170 @@ resource "aws_internet_gateway" "mwaa_igw" {
   }
 }
 
-# Create Public Subnet 1.
+# Elastic IPs for NAT Gateways
+resource "aws_eip" "nat1_eip" {
+  domain = "vpc"
+}
+
+resource "aws_eip" "nat2_eip" {
+  domain = "vpc"
+}
+
+# Public Subnets
 resource "aws_subnet" "public_subnet_1" {
   vpc_id            = aws_vpc.mwaa_vpc.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "${var.region}a"
+  map_public_ip_on_launch = true
+
   tags = {
-    Name = var.tag
+    Name = "${var.tag}-public-1"
   }
 }
 
-# Create Public Subnet 2.
 resource "aws_subnet" "public_subnet_2" {
   vpc_id            = aws_vpc.mwaa_vpc.id
   cidr_block        = "10.0.4.0/24"
   availability_zone = "${var.region}b"
+  map_public_ip_on_launch = true
+
   tags = {
-    Name = var.tag
+    Name = "${var.tag}-public-2"
   }
 }
 
-# Create Private Subnet 1.
+# Private Subnets
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.mwaa_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "${var.region}a"
   tags = {
-    Name = var.tag
+    Name = "${var.tag}-private-1"
   }
 }
 
-# Create Private Subnet 2.
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.mwaa_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "${var.region}b"
+
   tags = {
-    Name = var.tag
+    Name = "${var.tag}-private-2"
   }
 }
 
-# Create Route Table for Public Subnets.
+# NAT Gateways
+resource "aws_nat_gateway" "nat_gw_1" {
+  allocation_id = aws_eip.nat1_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "${var.tag}-natgw-1"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw_2" {
+  allocation_id = aws_eip.nat2_eip.id
+  subnet_id     = aws_subnet.public_subnet_2.id
+
+  tags = {
+    Name = "${var.tag}-natgw-2"
+  }
+}
+
+# Route Tables
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.mwaa_vpc.id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.mwaa_igw.id
   }
+
   tags = {
-    Name = var.tag
+    Name = "${var.tag}-public-rt"
   }
 }
 
-# Associate Public Subnet 1 with the Route Table.
-resource "aws_route_table_association" "public_subnet_1_assoc" {
+resource "aws_route_table" "private_route_table_1" {
+  vpc_id = aws_vpc.mwaa_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw_1.id
+  }
+
+  tags = {
+    Name = "${var.tag}-private-rt-1"
+  }
+}
+
+resource "aws_route_table" "private_route_table_2" {
+  vpc_id = aws_vpc.mwaa_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw_2.id
+  }
+
+  tags = {
+    Name = "${var.tag}-private-rt-2"
+  }
+}
+
+# Subnet Associations
+resource "aws_route_table_association" "public_assoc_1" {
   subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
-# Associate Public Subnet 2 with the Route Table.
-resource "aws_route_table_association" "public_subnet_2_assoc" {
+resource "aws_route_table_association" "public_assoc_2" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
-# Create Security Group for MWAA.
+resource "aws_route_table_association" "private_assoc_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table_1.id
+}
+
+resource "aws_route_table_association" "private_assoc_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table_2.id
+}
+
+# Security Group
 resource "aws_security_group" "mwaa_sg" {
-  vpc_id = aws_vpc.mwaa_vpc.id
+  name        = "${var.tag}-sg"
+  description = "Security group for MWAA access"
+  vpc_id      = aws_vpc.mwaa_vpc.id
+
+  # Allows HTTPS access from your personal IP
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = [var.personal_public_ip]
   }
+
+  # Allows inbound trafffic between components from the same SG (key rule for MWAA)
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    self            = true  # <- this creates the self-reference rule
+  }
+
+  # Outbound traffic to internet
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
-    Name = var.tag
+    Name = "${var.tag}-sg"
   }
 }
+
